4c161f5517848d99c2bfc1c5d2198bdc4cb31c69
wessam611 <wessam611@live.com>
2020-08-28 18:47:13 +0200

    added box_refine to pipeline

--- pipeline.py
+++ pipeline.py
@@ -4,6 +4,22 @@ import numpy as np
 from config import *
 from numba import prange
 
+
+def _sort_src_from_dst(src, dst):
+    ret = []
+
+    for point in dst:
+        min_dist = 50000
+        min_p = None
+        for p in src:
+            dist = abs(point-p).sum()
+            if  dist < min_dist:
+                min_dist = dist
+                min_p = p
+        ret.append(min_p.tolist())   
+    ret = np.float32(ret); dst = np.float32(dst)
+    return ret, dst
+
 def preprocess(img):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     img = cv2.copyMakeBorder( img, 15, 15, 15, 15, cv2.BORDER_CONSTANT) # to get a better countour
@@ -23,19 +39,9 @@ def rotate(img):
         if area > 300:
             epsilon = 0.1*cv2.arcLength(contour,True)
             approx = cv2.approxPolyDP(contour,epsilon,True)
-    src = []
-    dst = [[0, 0], [0, 255], [255, 0], [255, 255]]
 
-    for point in dst:
-        min_dist = 50000
-        min_p = None
-        for p in approx:
-            dist = abs(point-p).sum()
-            if  dist < min_dist:
-                min_dist = dist
-                min_p = p
-        src.append(min_p.tolist()[0])   
-    src = np.float32(src); dst = np.float32(dst)
+    dst = [[0, 0], [0, 255], [255, 0], [255, 255]]
+    src, dst = _sort_src_from_dst(approx, dst)
     
     M = cv2.getPerspectiveTransform(src, dst)
     img = cv2.warpPerspective(img,M,tuple(IMAGE_SIZE))
@@ -46,7 +52,7 @@ def rotate(img):
 
 
 def boxes_divide(img):
-    boxes = np.zeros((81, *BOX_SIZE))
+    boxes = np.zeros((81, *BOX_SIZE), dtype='uint8')
     for i in prange(81):
         rows = IMAGE_SIZE[0]//9; cols = IMAGE_SIZE[1]//9
         r = i//9
@@ -55,5 +61,28 @@ def boxes_divide(img):
         box = img[max(0, r*rows - margin): min(IMAGE_SIZE[0], (r+1)*rows+ margin), \
             max(0, c*cols-margin): min(IMAGE_SIZE[1], (c+1)*cols+margin)]
         box = cv2.resize(box, tuple(BOX_SIZE))
+        boxes[i] = box
+    return boxes
+
+def boxes_refine(boxes):
+    for i in prange(81):
+        box = boxes[i]
+        contours, hierarchy = cv2.findContours(box, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        max_area = 0
+        for contour in contours:
+            area = cv2.contourArea(contour)
+            if area > max_area:
+                max_area = area
+                rect = cv2.minAreaRect(contour)
+                rect = cv2.boxPoints(rect)
+
+        dst = [[0, 0], [0, 28], [28, 0], [28, 28]]
+        src, dst = _sort_src_from_dst(rect, dst)
+
+        M = cv2.getPerspectiveTransform(src, dst)
+        box = cv2.warpPerspective(box, M, tuple(BOX_SIZE))
+        box = cv2.adaptiveThreshold(box, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
+                cv2.THRESH_BINARY, 11, 2)
+
         boxes[i] = box
     return boxes
